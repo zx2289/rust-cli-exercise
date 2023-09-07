223340a2dd35ee9afa6435bb655a5b9591d0a54c
22890 <18392796709@163.com>
2023-09-07 21:05:49 +0800

    Refactor main.rs and add new files
    The main.rs file in the "grep" directory was modified to include additional imports and a new code block for parameter parsing. The file now imports `std::env` and a new module called `terminal::param`. The `main()` function was updated to parse command line arguments and store them in a `Param` struct. The parsed parameters are then printed. Some error handling was also added to handle cases where required arguments are missing.
    
    In addition to the modifications in main.rs, two new files were added. One is the `param.rs` module inside the `terminal` directory, which defines the `Param` struct and its implementation. The other is the `mod.rs` file inside the `terminal` directory, which exposes the `param` module.
    
    These changes refactor the code to separate parameter parsing into its own module for better organization and reusability.

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+mod terminal;
+
+use terminal::param::{Param};
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    //获取到参数之后 按照 搜索内容，文件  的格式保存参数
+    let param_option = Param::new(&args);
+    let param:Param;
+    match param_option {
+        Some(some) => {println!("参数解析成功");
+            param = some},
+        None => return,
+    }
+
+    println!("参数解析结果{:?}",param);
 }
---
+++ grep/src/terminal/param.rs
@@ -0,0 +1,27 @@
+
+
+#[derive(Debug)]
+pub struct Param<'a>{
+    search_value:&'a String,
+    search_source:&'a String
+}
+impl<'a>  Param<'a> {
+    pub  fn new (args:&'a Vec<String> ) -> Option<Param<'a>>{ 
+        let exec_path = args.get(0);
+        let search:&String;
+        let search_from:&String;
+
+        match args.get(1) {
+            Some(v) => search = v,
+            None => {println!("缺少必要参数");return None;},
+        }
+        match args.get(2) {
+            Some(v) => search_from = v,
+            None => {println!("缺少必要参数");return None;},
+        }
+        Option::Some( Param{
+            search_value:search,
+            search_source:search_from
+        })
+    }
+}
