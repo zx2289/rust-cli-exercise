59c881c396187b4197ba3e14aaaf3450fee45e42
22890 <18392796709@163.com>
2023-09-19 08:58:51 +0800

    指令参数解析

--- grep/src/terminal/param.rs
+++ grep/src/terminal/param.rs
@@ -1,27 +1,54 @@
-
+use std::collections::HashMap;
 
 #[derive(Debug)]
-pub struct Param<'a>{
-    search_value:&'a String,
-    search_source:&'a String
+pub struct Param<'a> {
+    search_value: &'a String,
+    search_source: &'a String,
+    instruct: Box<Vec<HashMap<&'a String, &'a String>>>,
 }
-impl<'a>  Param<'a> {
-    pub  fn new (args:&'a Vec<String> ) -> Option<Param<'a>>{ 
-        let exec_path = args.get(0);
-        let search:&String;
-        let search_from:&String;
 
-        match args.get(1) {
-            Some(v) => search = v,
-            None => {println!("缺少必要参数");return None;},
+impl<'a> Param<'a> {
+    pub fn new(args: &'a Vec<String>) -> Option<Param<'a>> {
+        let exec_path = args.get(0);
+        let mut search: Option<&String> = None;
+        let mut search_from: Option<&String> = None;
+        let mut instruct_vec: Vec<HashMap<&'a String, &'a String>> = Vec::new();
+        let mut dealed_index:usize = 0;
+        for (i, instruct) in args.iter().enumerate() {
+            if i == 0 || dealed_index == i {
+                continue;
+            }
+            if instruct.starts_with("-") {
+                let uppercase_v = instruct.to_uppercase();
+                // 这个是一个指令参数 对于不同的指令参数进行不同的处理
+                if uppercase_v == "-C" || uppercase_v == "-A" {
+                    let c_param = args.get(i + 1);
+                    match c_param {
+                        Some(v) => {
+                            let mut map: HashMap<&'a String, &'a String> = HashMap::new();
+                            map.insert(instruct, v);
+                            instruct_vec.push(map);
+                            dealed_index = i+1;
+                        }
+                        None => {
+                            println!("{:?} 指令缺少必要参数", instruct);
+                            return None;
+                        }
+                    }
+                }
+            } else {
+                if let Some(v) = search {
+                    search_from = Option::from(instruct);
+                } else {
+                    search = Option::from(instruct);
+                }
+            }
         }
-        match args.get(2) {
-            Some(v) => search_from = v,
-            None => {println!("缺少必要参数");return None;},
-        }
-        Option::Some( Param{
-            search_value:search,
-            search_source:search_from
+
+        Option::Some(Param {
+            search_value: search.unwrap(),
+            search_source: search_from.unwrap(),
+            instruct: Box::new(instruct_vec),
         })
     }
 
